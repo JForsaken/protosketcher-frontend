2039a10f0b727b4e36cd8b2c37a1c09e07215bc2
Francois Lauzier <francoislauzier1@gmail.com>
2017-01-23 16:48:49 -0500

    Added base mouse events to show/hide the menu

--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -1,10 +1,11 @@
 /* Node modules */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 /* Components */
 import RadialMenu from '../common/RadialMenu/RadialMenu';
 
-export default class Workspace extends Component {
+class Workspace extends Component {
 
   constructor(props, context) {
     super(props, context);
@@ -17,19 +18,35 @@ export default class Workspace extends Component {
     };
   }
 
-  toggleMenu(state) {
-    this.setState({ showMenu: state });
+  toggleMenu(e) {
+    if (e.type === 'mousedown') {
+      this.props.application.menu = {
+        pageX: e.pageX,
+        pageY: e.pageY,
+      };
+      this.setState({ showMenu: true });
+    } else if (e.type === 'mouseup' || (e.type === 'mouseleave'
+        && e.target.classList.contains('workspace-container'))) {
+      this.props.application.menu = {};
+      this.setState({ showMenu: false });
+    }
   }
 
   render() {
     return (
       <div
         className="workspace-container"
-        onMouseDown={() => this.toggleMenu(true)}
-        onMouseUp={() => this.toggleMenu(false)}
+        onMouseDown={this.toggleMenu}
+        onMouseUp={this.toggleMenu}
+        onMouseLeave={this.toggleMenu}
       >
       {this.state.showMenu && <RadialMenu />}
       </div>
       );
   }
 }
+
+export default connect(
+  ({ application }) => ({ application }),
+  null
+)(Workspace);
--- src/components/common/RadialMenu/RadialMenu.jsx
+++ src/components/common/RadialMenu/RadialMenu.jsx
@@ -22,26 +22,35 @@ class RadialMenu extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      opened: false,
       selectedIndex: -1,
     };
   }
 
   render() {
-    const { opened } = this.state.opened;
+    const menuStyle = {
+      height: 200,
+      left: this.props.application.menu.pageX - 100,
+      position: 'absolute',
+      top: this.props.application.menu.pageY - 100,
+      width: 200,
+    };
 
     return (
-      <div>
-        <span>Opened : {opened} </span>
-      {
-        menuItems.map((item, i) =>
-          <RadialMenuItem
-            {...item}
-            key={i}
-              /* Constants */
-            // import onClick={this.state.expanded ? this.toggleNav : null}
-          />)
-      }
+      <div style={menuStyle}>
+        <svg>
+          <circle cx="100" cy="100" r="25" />
+        </svg>
+        <div>
+        {
+          menuItems.map((item, i) =>
+            <RadialMenuItem
+              {...item}
+              key={i}
+                /* Constants */
+              // import onClick={this.state.expanded ? this.toggleNav : null}
+            />)
+        }
+        </div>
       </div>
     );
   }
