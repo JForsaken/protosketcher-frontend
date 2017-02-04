e577ba66e3cd14cc92238fa8d79a6dc26b8b20b2
GLatendresse <gabriel.latendresse@gmail.com>
2017-02-04 18:27:35 -0500

    basic drawing (#25)
    * Drawing implementation.

--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -9,6 +9,9 @@ import iconText from '../../../assets/images/icons/text-fields.png';
 /* Components */
 import RadialMenu from '../common/RadialMenu/RadialMenu';
 
+let offsetTop;
+let offsetLeft;
+
 const menuItems = [
   {
     color: '#F44336',
@@ -31,35 +34,31 @@ class Workspace extends Component {
     super(props, context);
 
     this.toggleMenu = this.toggleMenu.bind(this);
-    this.touchStart = this.touchStart.bind(this);
-    this.touchEnd = this.touchEnd.bind(this);
-    this.touchMove = this.touchMove.bind(this);
+    this.onStartingEvent = this.onStartingEvent.bind(this);
+    this.onEndingEvent = this.onEndingEvent.bind(this);
+    this.onMovingEvent = this.onMovingEvent.bind(this);
+    this.computeSvgPathString = this.computeSvgPathString.bind(this);
+    this.createSvgPathString = this.createSvgPathString.bind(this);
+    this.arePointsFeedable = this.arePointsFeedable.bind(this);
 
     this.state = {
       showMenu: false,
       mode: 0,
+      isDrawing: false,
+      svgPathStrings: [],
+      previousPoints: null,
     };
 
     this.touchTimer = 0;
   }
 
-  toggleMenu(e) {
-    if (e.preventDefault) e.preventDefault();
-    if (e.stopPropagation) e.stopPropagation();
-
-    if (e.type === 'contextmenu' || e.type === 'touchstart') {
-      this.props.application.touchStartPos = {
-        clientX: e.clientX,
-        clientY: e.clientY,
-      };
-      this.setState({ showMenu: true });
-    } else if (e.type === 'touchend' || e.type === 'touchcancel'
-      || (e.type === 'mouseleave' && e.target.classList.contains('workspace-container'))) {
-      this.setState({ showMenu: false });
-    }
+  componentDidMount() {
+    const workspaceElement = document.getElementById('workspace');
+    offsetTop = workspaceElement.offsetTop;
+    offsetLeft = workspaceElement.offsetLeft;
   }
 
-  touchStart(e) {
+  onStartingEvent(e) {
     e.stopPropagation();
 
     if (e.type === 'touchstart') {
@@ -79,9 +78,25 @@ class Workspace extends Component {
       ...this.props.application.touchStartPos,
     };
     this.touchTimer = setTimeout(() => this.toggleMenu(evt), 500);
+
+    // Start Drawing
+    let pointer = e;
+    if (e.type.substring(0, 5) === 'touch') {
+      pointer = e.touches[0];
+    }
+    const points = {
+      x: pointer.pageX - offsetLeft,
+      y: pointer.pageY - offsetTop,
+    };
+    this.setState({
+      isDrawing: true,
+      previousPoints: points,
+    });
+    this.createSvgPathString();
+    this.computeSvgPathString(points, 'M');
   }
 
-  touchEnd(e) {
+  onEndingEvent(e) {
     e.preventDefault();
     e.stopPropagation();
     const evt = {
@@ -93,9 +108,15 @@ class Workspace extends Component {
     if (this.touchTimer) {
       clearTimeout(this.touchTimer);
     }
+
+    // Stop Drawing
+    this.setState({
+      isDrawing: false,
+      previousPoints: null,
+    });
   }
 
-  touchMove(e) {
+  onMovingEvent(e) {
     e.preventDefault();
     e.stopPropagation();
     let deltaX = 0;
@@ -122,24 +143,97 @@ class Workspace extends Component {
         }
       }
     }
+
+    // Drawing
+    if (!this.state.showMenu) {
+      let pointer = e;
+      if (e.type.substring(0, 5) === 'touch') {
+        pointer = e.touches[0];
+      }
+      const points = {
+        x: pointer.pageX - offsetLeft,
+        y: pointer.pageY - offsetTop,
+      };
+      if (this.arePointsFeedable(points)) {
+        this.computeSvgPathString(points, 'L');
+        this.setState({
+          previousPoints: points,
+        });
+      }
+    }
+  }
+
+  arePointsFeedable(currentPoints) {
+    const minDistance = 10;
+    const a = this.state.previousPoints.x - currentPoints.x;
+    const b = this.state.previousPoints.y - currentPoints.y;
+    const c = Math.sqrt(a * a + b * b);
+    return c > minDistance;
+  }
+
+  createSvgPathString() {
+    const paths = this.state.svgPathStrings;
+    const minimumStringLength = 20;
+    if (paths.length > 0 && paths[paths.length - 1].length < minimumStringLength) {
+      paths.pop();
+    }
+    paths.push('');
+    this.setState({
+      svgPathStrings: paths,
+    });
+  }
+
+  computeSvgPathString(points, prefix) {
+    const pathStrings = this.state.svgPathStrings;
+    pathStrings[pathStrings.length - 1] += `${prefix}${points.x} ${points.y} `;
+    this.setState({
+      svgPathStrings: pathStrings,
+    });
+  }
+
+  toggleMenu(e) {
+    if (e.preventDefault) e.preventDefault();
+    if (e.stopPropagation) e.stopPropagation();
+
+    if (e.type === 'contextmenu' || e.type === 'touchstart') {
+      this.props.application.touchStartPos = {
+        clientX: e.clientX,
+        clientY: e.clientY,
+      };
+      this.setState({ showMenu: true });
+    } else if (e.type === 'touchend' || e.type === 'touchcancel'
+      || (e.type === 'mouseleave' && e.target.classList.contains('workspace-container'))) {
+      this.setState({ showMenu: false });
+    }
   }
 
   render() {
+    const svgPaths = [];
+    for (let i = 0; i < this.state.svgPathStrings.length; i++) {
+      svgPaths.push(
+        <path d={this.state.svgPathStrings[i]} stroke="blue" strokeWidth="3" fill="none">
+        </path>);
+    }
     return (
       <div
+        id="workspace"
         className="workspace-container"
-        onMouseDown={this.touchStart}
-        onMouseMove={this.touchMove}
-        onMouseUp={this.touchEnd}
+        onMouseDown={this.onStartingEvent}
+        onMouseMove={this.onMovingEvent}
+        onMouseUp={this.onEndingEvent}
+        onMouseLeave={this.onEndingEvent}
         onMouseLeave={this.toggleMenu}
-        onTouchStart={this.touchStart}
-        onTouchMove={this.touchMove}
-        onTouchEnd={this.touchEnd}
+        onTouchStart={this.onStartingEvent}
+        onTouchMove={this.onMovingEvent}
+        onTouchEnd={this.onEndingEvent}
         onContextMenu={this.toggleMenu}
       >
       {this.state.showMenu && <RadialMenu items={menuItems} offset={Math.PI / 4} />}
+        <svg height="100%" width="100%">
+          {svgPaths}
+        </svg>
       </div>
-      );
+    );
   }
 }
 
