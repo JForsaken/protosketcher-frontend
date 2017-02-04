0db2027edb107aae3f89521ab2677a2e3d9eafe9
Francois Lauzier <francoislauzier1@gmail.com>
2017-02-04 17:08:06 -0500

    Fixed fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'babel-polyfill',
     'webpack-hot-middleware/client',
-    './src/index'
+    './src/index',
   ],
   output: {
     filename: 'app.js',
     path: path.join(__dirname, 'dist'),
-    publicPath: '/assets/'
+    publicPath: '/assets/',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('development')
+        NODE_ENV: JSON.stringify('development'),
       },
-      '__DEVTOOLS__': process.env.DEVTOOLS === 'true' ? true : false
+      __DEVTOOLS__: process.env.DEVTOOLS === 'true',
     }),
     new HtmlWebpackPlugin({
       title: 'Protosketcher',
       filename: 'index.html',
       template: 'index.template.html',
-      favicon: path.join(__dirname, 'assets', 'images', 'favicon.ico')
+      favicon: path.join(__dirname, 'assets', 'images', 'favicon.ico'),
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
@@ -41,16 +41,13 @@ module.exports = {
     loaders: [
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
+        loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'],
       },
       { test: /\.css$/,
         loader: 'style-loader!css-loader!cssnext-loader',
       },
-      { test: /\.(ttf|otf|eot)$/,
-        loader:'file-loader?name=fonts/[name].[ext]',
-      },
       {
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        test: /\.(eot|otf|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
         loader: 'url?limit=100000@name=[name][ext]',
       },
       {
