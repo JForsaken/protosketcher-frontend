18cf8439f0b1351f559092b51896486d15190573
Jonathan Rainville <rainville.jonathan@gmail.com>
2017-02-17 12:52:28 -0500

    add slideshow skeleton

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,15 +3,18 @@ import React, { Component } from 'react';
 
 /* Components */
 import LandingMenu from './LandingMenu/LandingMenu';
+import Slideshow from './Slideshow/Slideshow';
 
 export default class LandingPage extends Component {
   render() {
     return (
       <div className="landing-page">
         <LandingMenu />
-
-        <div className="container">
-          <h1>Protosketcher</h1>
+        <div className="container page-container">
+          <div className="slideshow">
+            <h1>Protosketcher</h1>
+          </div>
+          <Slideshow />
         </div>
       </div>
     );
