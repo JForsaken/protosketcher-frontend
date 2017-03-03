c1e0245e18a862937b0a718c68b8c74b7e013e7f
Jonathan Rainville <rainville.jonathan@gmail.com>
2017-03-03 12:08:59 -0500

    ad api reducer for delete prototype

--- src/reducers/api.js
+++ src/reducers/api.js
@@ -24,6 +24,11 @@ const initialState = {
     time: null,
     error: {},
   },
+  deletePrototype: {
+    prototypes: {},
+    time: null,
+    error: {},
+  },
   getPages: {
     pages: {},
     time: null,
@@ -142,6 +147,14 @@ const actionHandlers = {
     lastAction: action.type,
     createPrototype: {
       prototypes: action.prototype,
+      time: action.time,
+      error: action.error,
+    },
+  }),
+  [constants.DELETE_PROTOTYPE]: (state, action) => ({
+    lastAction: action.type,
+    deletePrototype: {
+      prototypes: action.prototypes,
       time: action.time,
       error: action.error,
     },
