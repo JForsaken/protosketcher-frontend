24b86c119318f3f92299659500a2bbfbf3cbbfed
Gabriel Latendresse <glatendresse@localhost.localdomain>
2017-03-25 16:58:27 -0400

    little adjustment

--- src/components/Workspace/helpers/copyPaste.js
+++ src/components/Workspace/helpers/copyPaste.js
@@ -81,8 +81,8 @@ export function computeCopyPasteOffset() {
   } else {
     const workspaceBox = this.workspace.getBoundingClientRect();
     pastePosition = {
-      x: Math.round((workspaceBox.width) / 2),
-      y: Math.round((workspaceBox.height) / 2),
+      x: Math.round(workspaceBox.width / 2),
+      y: Math.round(workspaceBox.height / 2),
     };
   }
 
