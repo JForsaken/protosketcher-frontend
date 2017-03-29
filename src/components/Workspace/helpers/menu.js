562379bd34d251cc3e77f5eb8df5ababc2e65703
Justin D'Errico <justin.derrico1991@gmail.com>
2017-03-28 20:20:27 -0400

    fix

--- src/components/Workspace/helpers/menu.js
+++ src/components/Workspace/helpers/menu.js
@@ -144,6 +144,7 @@ export function doAction(point) {
       this.shapesClipboard = {};
       this.textsClipboard = {};
 
+      this.centralSelectionPoint = this.getCentralPointOfSelection();
       this.setState({ ...items });
       break;
     }
