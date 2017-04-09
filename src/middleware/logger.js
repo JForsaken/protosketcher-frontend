05243b21aeccbdc9ab7ae932acea80b9a1f3eb77
Justin D'Errico <justin.derrico1991@gmail.com>
2017-04-09 17:27:40 -0400

    comments

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,2 +1,16 @@
 /* eslint-disable no-console */
 export default () => next => action => next(action);
+
+/* If you want logs back on...
+export default function logger({ getState }) {
+  return next => action => {
+    console.group();
+    console.log('will dispatch', action);
+    const result = next(action);
+    console.log('state after dispatch', getState());
+    console.groupEnd();
+
+    return result;
+  };
+}
+*/
